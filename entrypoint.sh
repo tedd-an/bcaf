d60c704f486b8378bf0684707ad9b3acd8bd0a48
Tedd Ho-Jeong An <tedd.an@intel.com>
2022-11-07 20:45:16 -0800

    Fix input params for sync_patchwork

--- entrypoint.sh
+++ entrypoint.sh
@@ -124,7 +124,7 @@ case $TASK in
             set_git_safe_dir $GITHUB_WORKSPACE
             update_github_token $GITHUB_REPOSITORY
             # calling sync_patchwork.py
-            /sync_patchwork.py -c /config.json -b $WORKFLOW -r $GITHUB_REPOSITORY -t $SPACE -s $GITHUB_WORKSPACE
+            /sync_patchwork.py -c /config.json -b $WORKFLOW -s $GITHUB_WORKSPACE user $GITHUB_REPOSITORY
         ;;
     ci|CI|Ci)
         echo "Task: CI"
